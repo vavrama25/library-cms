35df1a30c0a5f213eb3ac5407de2bbcf8b8b257c
Martin Vavra <vavrama25@sps-prosek.cz>
2026-08-30 19:07:51 +0200

    db

--- cms-martin.sql
+++ cms-martin.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Počítač: 127.0.0.1
--- Vytvořeno: Pon 24. srp 2026, 10:25
+-- Vytvořeno: Ned 30. srp 2026, 19:06
 -- Verze serveru: 10.4.24-MariaDB
 -- Verze PHP: 8.1.4
 
@@ -33,23 +33,54 @@ CREATE TABLE `cms-content` (
   `autor` varchar(100) NOT NULL,
   `genre` varchar(100) NOT NULL,
   `form` set('beletrie','naucna') NOT NULL,
-  `imgLink` varchar(150) DEFAULT NULL,
+  `imgLink` varchar(2048) DEFAULT NULL,
   `description` varchar(1000) NOT NULL,
   `availability` tinyint(1) UNSIGNED NOT NULL DEFAULT 1,
   `price` int(11) NOT NULL,
-  `timestamp` timestamp NULL DEFAULT current_timestamp()
+  `timestamp` timestamp NULL DEFAULT current_timestamp(),
+  `deleted` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Vypisuji data pro tabulku `cms-content`
 --
 
-INSERT INTO `cms-content` (`ID_cms-content`, `title`, `autor`, `genre`, `form`, `imgLink`, `description`, `availability`, `price`, `timestamp`) VALUES
-(4, 'Mein kampf', 'Adolf Hitler', 'autobiografie', 'naucna', 'https://web2.mlp.cz/koweb/00/03/44/02/Small.43.jpg', 'Kniha francouzského novináře a dokumentaristy Antoina Vitkina Mein Kampf se snaží popsat vznik a následnou recepci Hitlerova spisu. Vitkine svou knihu rozdělil do dvou částí. V první – Před válkou: „Nacistická bible“ – líčí okolnosti vzniku Mein Kampfu, shrnuje jeho základní teze a popisuje, jaký význam tato kniha získala za Třetí říše. V druhé části knihy – Po válce: Historie bez konce – Vitkine popisuje jednak to, jak se s dědictvím Mého boje vyrovnávali jednotlivé generace v poválečném Německu, jednak jeho další osudy ve světě (do kolika jazyků byl přeložen, v jakých nákladech atd.). Vitkine se zvlášť zaměřuje na arabský svět, v němž se Mému boji po založení státu Izrael dostalo velkého ohlasu. Monografie končí výčtem sedmi ponaučení pro současnost, jaké bychom si z této nebezpečné knihy měli stále brát. Nakladatelská anotace.', 1, 555, '2026-07-11 10:39:50'),
-(5, 'Bejcek', 'Matej Dufek', 'SCIFI', 'beletrie', 'https://cdn.pixabay.com/photo/2023/10/18/10/31/bull-8323682_960_720.jpg 1x, https://cdn.pixabay.com/photo/2023/10/18/10/31/bull-8323682_1280.jpg ', 'BEJK', 1, 500, '2026-08-18 15:37:50'),
-(6, 'Hitlerova nenávist k Židům', 'Reuth, Ralf Georg', '-', 'beletrie', 'https://web2.mlp.cz/koweb/00/03/56/68/Small.44.jpg', 'Německý historik se ve své práci snaží zpochybnit historiky (Kershaw, Fest, Bullock atd.) zastávaný názor, že počátky Hitlerovy nenávisti vůči Židům spadají do doby jeho pobytu ve Vídni před první světovou válkou. Na základě pramenů a literatury se autor snaží dokázat, že Hitlerův antisemitismus se zrodil až během událostí roku 1919; v tomto roce čelí Německo spartakovskému povstání, v Mnichově je vyhlášena Bavorská republika rad (trvala od dubna do května 1919) a Němci jsou nuceni přijmout tvrdé podmínky versailleské mírové smlouvy. Hitler podle Reutha vidí úzkou spojitost mezi Židy a bolševiky, k nimž cítí bytostný odpor. Kniha mapuje vývoj Hitlerova postoje k Židům od jeho pobytu ve Vídni až do jeho rozhodnutí k provedení genocidy evropských Židů roku 1941.', 1, 500, '2026-08-18 15:52:09'),
-(7, 'HOVNO', 'HOnza studnicka', 'Fantasyaaaa', 'beletrie', 'https://web2.mlp.cz/koweb/00/05/27/10/Small.61.jpg', 'Kniha o hovne', 1, 900, '2026-08-20 06:44:54'),
-(8, 'Všechno při starém', ' Claire Lombardo', 'Beletrie pro dospělé čtenáře a mládež v češtině.', 'beletrie', 'https://web2.mlp.cz/koweb/00/05/27/10/Small.57.jpg', 'Julia Amesová má konečně pocit, že svůj život drží pevně v rukou. Po mládí plném otřesů a citových zmatků dospěla do zdánlivě klidné fáze – do bezpečného, usazeného středu života. V sedmapadesáti letech věří, že to nejhorší má za sebou. Jenže pak se jí v životě začnou objevovat věci, které nečekala: Překvapivé oznámení syna. Odloučení svéhlavé dospívající dcery. A především návrat minulosti, který je stejně svůdný jako nebezpečný. Stačí málo a Julia se může znovu ocitnout na hraně, kde už jednou byla. Všechno při starém je pronikavým a citlivým portrétem života v celé jeho neuhlazenosti. S mimořádnou přesností zachycuje mateřství bez příkras, křehkost mezigeneračních vztahů i nepředvídatelné řetězce příčin a následků, jež formují naše osudy. Je to příběh o tom, jak se proměňujeme – a jak nás minulost nikdy úplně nepustí. Nakladatelská anotace.', 1, 399, '2026-08-22 09:13:58');
+INSERT INTO `cms-content` (`ID_cms-content`, `title`, `autor`, `genre`, `form`, `imgLink`, `description`, `availability`, `price`, `timestamp`, `deleted`) VALUES
+(28, 'Duna', 'Frank Herbert', 'Sci-fi', 'beletrie', 'https://covers.openlibrary.org/b/id/10210554-L.jpg', 'Mocenské boje o nejdůležitější surovinu ve vesmíru na nehostinné pouštní planetě Arrakis.', 5, 299, '2026-08-29 12:25:05', 0),
+(29, 'Nadace', 'Isaac Asimov', 'Sci-fi', 'beletrie', 'https://www.databazeknih.cz/img/books/23_/231916/nadace-231916.webp?v=1417720226', 'Pád galaktického impéria a snaha o záchranu lidského vědění prostřednictvím psychohistorie.', 5, 299, '2026-08-29 12:25:05', 0),
+(30, 'Problém tří těles', 'Liou Cch\'-sin', 'Sci-fi', 'beletrie', 'https://www.databazeknih.cz/img/books/33_/334860/problem-tri-teles-jPU-334860.webp?v=1496177237', 'První kontakt lidstva s mimozemskou civilizací na pozadí čínské kulturní revoluce.', 5, 299, '2026-08-29 12:25:05', 0),
+(31, 'Marťan', 'Andy Weir', 'Sci-fi', 'beletrie', 'https://www.databazeknih.cz/img/books/22_/225946/martan-3Bs-225946.webp?v=1421148026', 'Boj o přežití astronauta Marka Watneyho, který zůstal omylem opuštěn na Marsu.', 5, 299, '2026-08-29 12:25:05', 0),
+(32, 'Spasitel', 'Andy Weir', 'Sci-fi', 'beletrie', 'https://www.databazeknih.cz/img/books/47_/476966/spasitel-TPX-476966.webp?v=1626431998', 'Osamělý astronaut se probouzí s amnézií a musí zachránit Zemi před vyhynutím.', 5, 299, '2026-08-29 12:25:05', 0),
+(33, 'Neuromancer', 'William Gibson', 'Sci-fi', 'beletrie', 'https://covers.openlibrary.org/b/id/283860-L.jpg', 'Kyberpunková klasika o zkrachovalém hackerovi najatém na zdánlivě nemožnou misi.', 5, 299, '2026-08-29 12:25:05', 0),
+(34, 'Sní androidi o elektrických ovečkách?', 'Philip K. Dick', 'Sci-fi', 'beletrie', 'https://www.databazeknih.cz/img/books/35_/352336/blade-runner-gFV-352336.webp?v=1505803880', 'Lov zběhlých androidů v postapokalyptickém světě, který posloužil jako předloha pro film Blade Runner.', 5, 299, '2026-08-29 12:25:05', 0),
+(35, 'Konec dětství', 'Arthur C. Clarke', 'Sci-fi', 'beletrie', 'https://www.databazeknih.cz/img/books/50_/506303/konec-detstvi-yNC-506303.webp?v=1669210538', 'Mimozemšťané přinesou Zemi utopii, ale za cenu ztráty lidské identity a budoucnosti.', 5, 299, '2026-08-29 12:25:05', 0),
+(36, 'Setkání s Rámou', 'Arthur C. Clarke', 'Sci-fi', 'beletrie', 'https://www.databazeknih.cz/img/books/49_/498805/setkani-s-ramou-uCy-498805.webp?v=1659143464', 'Průzkum obřího mimozemského artefaktu, který nečekaně vstoupí do sluneční soustavy.', 5, 299, '2026-08-29 12:25:05', 0),
+(37, 'Válka světů', 'H. G. Wells', 'Sci-fi', 'beletrie', 'https://www.databazeknih.cz/img/books/17_/1778/valka-svetu-UhN-1778.webp?v=1519733634', 'Klasický příběh o nemilosrdné invazi technologicky vyspělých Marťanů na Zemi.', 5, 299, '2026-08-29 12:25:05', 0),
+(38, 'Stroj času', 'H. G. Wells', 'Sci-fi', 'beletrie', 'https://www.databazeknih.cz/img/books/82_/8216/stroj-casu-X20-8216.webp?v=1519066775', 'Cesta do daleké budoucnosti, kde se lidstvo evolučně rozdělilo na dva odlišné druhy.', 5, 299, '2026-08-29 12:25:05', 0),
+(39, 'Den trifidů', 'John Wyndham', 'Sci-fi', 'beletrie', 'https://www.databazeknih.cz/img/books/19_/197/den-trifidu-65e21a7d53c5a.webp?v=1709316733', 'Většina lidstva oslepne a hrstka vidoucích musí čelit smrtícím, masožravým rostlinám.', 5, 299, '2026-08-29 12:25:05', 0),
+(40, 'Levá ruka tmy', 'Ursula K. Le Guin', 'Sci-fi', 'beletrie', 'https://www.databazeknih.cz/img/books/42_/42513/leva-ruka-tmy-vNQ-42513.webp?v=1605532863', 'Průzkumník se snaží pochopit společnost na ledové planetě, kde obyvatelé nemají stálé pohlaví.', 5, 299, '2026-08-29 12:25:05', 0),
+(41, 'Hyperion', 'Dan Simmons', 'Sci-fi', 'beletrie', 'https://covers.openlibrary.org/b/id/380332-L.jpg', 'Poutníci sdílejí své příběhy na cestě k tajemným Hrobkám času, zatímco hrozí intergalaktická válka.', 5, 299, '2026-08-29 12:25:05', 0),
+(42, 'Stopařův průvodce Galaxií', 'Douglas Adams', 'Sci-fi', 'beletrie', 'https://www.databazeknih.cz/img/books/39_/3996/stoparuv-pruvodce-galaxii-qT0-3996.webp?v=1639230769', 'Humoristická cesta vesmírem poté, co je Země zničena kvůli stavbě hyperprostorové dálnice.', 5, 299, '2026-08-29 12:25:05', 0),
+(43, 'Enderova hra', 'Orson Scott Card', 'Sci-fi', 'beletrie', 'https://www.databazeknih.cz/img/books/32_/322490/enderova-hra-Agm-322490.webp?v=1480405353', 'Nadaný chlapec je cvičen v orbitální vojenské akademii k odvrácení hrozící mimozemské invaze.', 5, 299, '2026-08-29 12:25:05', 0),
+(44, 'Hvězdná pěchota', 'Robert A. Heinlein', 'Sci-fi', 'beletrie', 'https://www.databazeknih.cz/img/books/12_/12846/hvezdna-pechota-DFI-12846.webp?v=1592668834', 'Vojenská sci-fi sledující drsný výcvik a boj elitních jednotek proti mimozemským broukům.', 5, 299, '2026-08-29 12:25:05', 0),
+(45, 'Fahrenheit 451', 'Ray Bradbury', 'Sci-fi', 'beletrie', 'https://covers.openlibrary.org/b/id/12993656-L.jpg', 'Dystopická vize společnosti, kde hasiči požáry nehasí, ale naopak pálí zakázané knihy.', 5, 299, '2026-08-29 12:25:05', 0),
+(46, 'Solaris', 'Stanisław Lem', 'Sci-fi', 'beletrie', 'https://covers.openlibrary.org/b/id/12313764-L.jpg', 'Psychologický střet lidských vědců s nepochopitelným, myslícím oceánem na cizí planetě.', 5, 299, '2026-08-29 12:25:05', 0),
+(47, 'Piknik u cesty', 'Arkadij a Boris Strugačtí', 'Sci-fi', 'beletrie', 'https://www.databazeknih.cz/img/books/46_/468037/piknik-u-cesty-boH-468037.webp?v=1615456153', 'Stalker proniká do smrtelně nebezpečného Pásma, aby získal podivné artefakty po mimozemské návštěvě.', 5, 299, '2026-08-29 12:25:05', 0),
+(48, 'Bohatý táta, chudý táta', 'Robert T. Kiyosaki', 'Osobní rozvoj', 'naucna', 'https://www.databazeknih.cz/img/books/42_/425338/bohaty-tata-chudy-tata-dj0-425338.webp?v=1582110048', 'Základy finanční gramotnosti a rozdíly v myšlení chudých a bohatých lidí.', 6, 350, '2026-08-29 12:44:28', 0),
+(49, 'Atomové návyky', 'James Clear', 'Osobní rozvoj', 'naucna', 'https://www.databazeknih.cz/img/books/43_/431187/atomove-navyky-yA1-431187.webp?v=1655487611', 'Praktický a vědecky podložený průvodce budováním dobrých zvyků.', 10, 400, '2026-08-29 12:44:28', 0),
+(50, 'Černá labuť', 'Nassim Nicholas Taleb', 'Ekonomie', 'naucna', 'https://www.databazeknih.cz/img/books/94_/94648/cerna-labut.webp?v=1743145165', 'Vliv vysoce nepravděpodobných událostí na náš život a globální ekonomiku.', 3, 499, '2026-08-29 12:44:28', 0),
+(51, 'Krátká historie téměř všeho', 'Bill Bryson', 'Věda', 'naucna', 'https://www.databazeknih.cz/img/books/41_/4160/kratka-historie-mytu-R8U-4160.webp?v=1587141211', 'Srozumitelně a vtipně podaný přehled o vzniku vesmíru, Země a života na ní.', 5, 450, '2026-08-29 12:44:28', 0),
+(52, 'Sobecký gen', 'Richard Dawkins', 'Biologie', 'naucna', 'https://www.databazeknih.cz/img/books/60_/607278/sobecky-gen.webp?v=1787161005', 'Revoluční pohled na evoluci z perspektivy genetiky a přirozeného výběru.', 2, 380, '2026-08-29 12:44:28', 0),
+(53, 'Proč spíme', 'Matthew Walker', 'Zdraví', 'naucna', 'https://www.databazeknih.cz/img/books/38_/383745/proc-spime-0GD-383745.webp?v=1530181072', 'Odhalení klíčového vlivu spánku na naše zdraví, imunitu a kognitivní schopnosti.', 8, 399, '2026-08-29 12:44:28', 0),
+(54, 'Jak získávat přátele a působit na lidi', 'Dale Carnegie', 'Psychologie', 'naucna', 'https://www.databazeknih.cz/img/books/28_/283302/jak-ziskavat-pratele-a-pusobit-na-l-nHL-283302.webp?v=1454778535', 'Klasická příručka pro zlepšení komunikačních a mezilidských dovedností.', 15, 290, '2026-08-29 12:44:28', 0),
+(55, 'Tělo sčítá rány', 'Bessel van der Kolk', 'Psychiatrie', 'naucna', 'https://www.databazeknih.cz/img/books/47_/471711/telo-scita-rany-9f7-471711.webp?v=1633258918', 'Studie o tom, jak trauma fyzicky i psychicky mění lidský mozek a tělo.', 4, 450, '2026-08-29 12:44:28', 0),
+(56, 'Ticho', 'Susan Cain', 'Psychologie', 'naucna', 'https://www.databazeknih.cz/img/books/14_/141794/ticho-sila-introvertu-ve-svete-kter-8KJ-141794.webp?v=1486594669', 'Síla introvertů ve světě, který nikdy nepřestává mluvit.', 7, 350, '2026-08-29 12:44:28', 0),
+(57, 'Vladař', 'Niccolò Machiavelli', 'Politologie', 'naucna', 'https://www.databazeknih.cz/img/books/29_/2946/vladar-9fR-2946.webp?v=1566996870', 'Historický traktát o politické moci, pragmatismu a vládnutí.', 4, 200, '2026-08-29 12:44:28', 0),
+(58, 'Umění války', 'Sun-c\'', 'Historie', 'naucna', 'https://www.databazeknih.cz/img/books/24_/249268/umeni-valky-249268.webp?v=1431528848', 'Starověký čínský manuál vojenské strategie a taktiky.', 12, 199, '2026-08-29 12:44:28', 0),
+(59, 'Guns, Germs, and Steel', 'Jared Diamond', 'Antropologie', 'naucna', 'https://covers.openlibrary.org/b/id/7884018-L.jpg', 'Evoluční a geografické vysvětlení vývoje lidských civilizací.', 3, 550, '2026-08-29 12:44:28', 0),
+(60, 'Slepá skvrna', 'Daniel Prokop', 'Sociologie', 'naucna', 'https://www.databazeknih.cz/img/books/41_/410685/slepe-skvrny-o-chudobe-vzdelavani-p-b1w-410685.webp?v=1571178631', 'Analýza české společnosti, chudoby a vzdělávání.', 9, 350, '2026-08-29 12:44:28', 0),
+(61, 'Životice: Obraz (po)zapomenuté tragédie', 'Karin Lednická', 'Historie', 'naucna', 'https://www.databazeknih.cz/img/books/48_/487282/zivotice-obraz-po-zapomenute-traged-cRu-487282.webp?v=1641049120', 'Rekonstrukce historických událostí v těšínské vesnici.', 6, 390, '2026-08-29 12:44:28', 0),
+(62, 'Krev, pot a pixely', 'Jason Schreier', 'IT', 'naucna', 'https://www.databazeknih.cz/img/books/40_/403587/krev-pot-a-pixely-pribehy-vitezstvi-dKa-403587.webp?v=1550573650', 'Pohled za oponu vývoje nejznámějších videoher.', 5, 349, '2026-08-29 12:44:28', 0);
 
 -- --------------------------------------------------------
 
@@ -71,23 +102,10 @@ CREATE TABLE `cms-login` (
 --
 
 INSERT INTO `cms-login` (`ID_login`, `email`, `password`, `deleted`, `role`, `credits`) VALUES
-(3, 'del_2026-08-20_del_2026-08-20_del_2026-08-20_a@AS.c', '581e2c1f7e57fd0604c61d8728abe171b0c3ecce14c481a4a06d223cf473bf8715f122bb4823b5a6d4cb74fd1f818b19fee4169b171ea316bcce73912f4', 1, 'admin', 200),
-(4, 'a@AS.cz', 'a743631d4e742d5defee93ae22586c176c827f2480458306db42d95798e714ff09d85f693e3844d9d5848009ab81eaaf6f403e53983fae25b98c1c2b763', 0, 'user', 0),
-(5, 'abc@abc.abc', '374375decc8f937d91cbd364911bf2f30bbe9d858226c1e844f7c1838f4237922602780a2670e3d9b1d327c438340f74f127f1464d98a88d86e33dde90c', 0, 'user', 0),
-(7, 'localhost@ahs.sas', 'c4a83517c4bc095c0ef4c015055843b84f6ffd6dee91f0f7f5e822d94be4aa4d89422233c3a7091816bc41547b466cdaa4de03887abceaa8f0743c63575', 0, 'user', 0),
-(8, 'localhost@ahs.sasa', '0337ba054cb9d3a7c7e8fc356afa991119d5f1784a4d8dfd3471e07924a7e7e6bfdd3bb426beb699ad1b4a1083b43ae3856f78e16ae0131badd27cdc6a2', 0, 'user', 0),
-(9, 'localhost@ahs.sasas', 'ca8fabeab9a212030ba759d44af30e9ec46191b5c8be2966b2716e529f8d0cb5296ed8a157b4bcec41f2569baaf77f3618a78f284ece77c5ba28b01a7ce', 0, 'user', 0),
-(10, 'localhost@ahs.cz', '5ec78bf47a886a42a9acbbd61785b41eb07da51df163f7f7b405740155e5c7512c5532b5611346e68d8fbd8230c2086e2ba3a4d37c60d843341b0e73200', 0, 'user', 0),
-(14, '1111@gmail.com', '1dfd7913e43e846b43da9ac80fb32fb2dcb33f34c059f4a6fafa75ebde545d8d36a18757abd9a502297ccae43977903535a4a0ce2af0f7e1550426c5976', 0, 'user', 0),
-(16, 'del_1781111274_vavrama25@sps-prosek.cz', 'ca82da7073d99f9d00b40c29a6e1efc5fc007f0aa01b85b77adcb6d359bceb034e3ebf69602e15720b3cdc433aaa4da632782c1fb0c903642e31cefa892', 1, 'user', 0),
-(17, 'del_2026-06-10T17:11:31+00:00_vavrama25@sps-prosek.cz', 'e901c5a7ea5dc7f8dd391ef1ae6ca877197f5291016b886299cd0313df618f3963d1b5c58e7a4b1f9597c64c505b4de4be51cc32536cda219149a045858', 1, 'user', 0),
-(18, 'del_2026-06-10_vavrama25@sps-prosek.cz', 'ef7fe00e267045c6ab61ca4050ccd4cfdffa2f88547dcb9078959f525fe4719b5839dab2b5fc6e63d91ce554166d3a352057cc024390cdd8db44f7b0f80', 1, 'user', 0),
-(19, 'vavrama25@sps-prosek.cz', '562295e360186dd00b6c474975a3c6735f64eddaa36b2449405eb4c4b94c5a31ac391a5c7f818ef40d14b2b0603155c0d90ccdeef87824e2f2383ce487e', 0, 'admin', 445),
-(20, 'vavrama25@sps-prosek.czff', 'e02dd6c42ddbe8b63f8320746b76a5717e3d0d892c66799f0bc5edc295d052861182787479718ee4a0ee3e7afa093033dcfcea3e1c7fa6e98a5b67d2002', 0, 'user', 0),
-(21, 'b@b.b', '0da2a8e7e47cff715e97641e8a05b619a0c13e95832665adeaab5c7e671e7dc646491f2f4b447e08e5467a02e2f1ab51fda11e207bcf35c9611ebef60f0', 0, 'user', 0),
-(22, 'a@a.a', 'f905e8f3092680fecfc1d3970a0fbfaea1af1f11aeb85dfaec1e3db892a1ea9c5a59ab04ebeab966a074cf455c3cb67a55e826952115fe76e329ddd85dc', 0, 'user', 0),
-(23, 'del_2026-07-10_martinvavra2010@gmail.com', '5adb6f6027d0afc86d06b077a1f3d88d8638a7f9742f77f393520a094823d6b7ba004f9d4ddda05a5c22db3ecce24de7a8cec41cd6367ac254719cbac43', 1, 'user', 0),
-(24, 'negr@negr.negr', '01d97a589d7d2a800591c0de6139e897feaee2f61e3f3b269d9b4931d15382418d51ea7618cf34a820bc2fe47d37b545271575ff49f1808f4900a6b0029', 0, 'user', 0);
+(19, 'vavrama25@sps-prosek.cz', '562295e360186dd00b6c474975a3c6735f64eddaa36b2449405eb4c4b94c5a31ac391a5c7f818ef40d14b2b0603155c0d90ccdeef87824e2f2383ce487e', 0, 'admin', 5000),
+(25, 'user@t.t', '9caa559834bceab5c03f1af79b2e1bb128e5d28f282bfea7aefacd70e012cf169f6b772b2641a8df844b3e8e160547714c6c7f9e30df82ab68ef1ac320a', 0, 'user', 5000),
+(26, 'worker@t.t', '976af98194918ef28f6d0cfc4d2acd952d10a86130846312d306edbd77e652708109f7d859be3743a8375f47470b5751b3b08145a1b6ac1ebb90a7e7954', 0, 'worker', 5000),
+(27, 'admin@t.t', '86ffbd5b4a674488cb9db4888ee24c324d425d6ca309ff65e9c4c8f10b5160f7dd1f538f4d12725ae091850608b2d7cfa131c52e482934129aa10eeea14', 0, 'user', 0);
 
 -- --------------------------------------------------------
 
@@ -101,13 +119,6 @@ CREATE TABLE `cms-reset_pass` (
   `token` varchar(100) COLLATE utf8_czech_ci NOT NULL,
   `date` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
-
---
--- Vypisuji data pro tabulku `cms-reset_pass`
---
-
-INSERT INTO `cms-reset_pass` (`ID_reset_pass`, `email`, `token`, `date`) VALUES
-(99, 'martinvavra2010@gmail.com', 'd2575e107dff27a0eddadbcd67ae192c29e31810d9f78da7d578c064f00add51fbfcdc9663d42ad166bdf4b6ae2fbc20', '2026-07-10 09:12:18');
 
 --
 -- Indexy pro exportované tabulky
@@ -139,13 +150,13 @@ ALTER TABLE `cms-reset_pass`
 -- AUTO_INCREMENT pro tabulku `cms-content`
 --
 ALTER TABLE `cms-content`
-  MODIFY `ID_cms-content` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `ID_cms-content` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
 
 --
 -- AUTO_INCREMENT pro tabulku `cms-login`
 --
 ALTER TABLE `cms-login`
-  MODIFY `ID_login` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `ID_login` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT pro tabulku `cms-reset_pass`
